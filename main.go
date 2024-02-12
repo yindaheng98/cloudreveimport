e6d9556e1f1a164077dbb147c328d97e018d997c
yindaheng98 <yindaheng98@163.com>
2024-02-11 17:23:35 -0800

    init main

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	model "github.com/cloudreve/Cloudreve/v3/models"
+	"github.com/cloudreve/Cloudreve/v3/pkg/conf"
+	"github.com/cloudreve/Cloudreve/v3/pkg/util"
+)
+
+var (
+	userName string
+	confPath string
+)
+
+func init() {
+	flag.StringVar(&confPath, "c", util.RelativePath("conf.ini"), "Path to the config file.")
+	flag.StringVar(&userName, "u", "admin@cloudreve.org", "Email of the target user.")
+	flag.Parse()
+	conf.Init(confPath)
+	model.Init()
+}
+
+func main() {
+	user, err := model.GetUserByEmail(userName)
+	if err != nil {
+		return
+	}
+	fmt.Printf("%v\n", user)
+}
