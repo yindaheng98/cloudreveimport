c7845a907629f67a6d7f8603816bfaba8bd7ff89
yindaheng98 <yindaheng98@163.com>
2024-02-11 22:28:56 -0800

    action

---
+++ README.md
@@ -0,0 +1,10 @@
+# cloudreveimport
+
+Import your existing files into cloudreve!
+
+## Install
+
+```sh
+go install github.com/yindaheng98/cloudreveimport
+pip install cloudreveimport
+```
---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+with open("README.md", "r", encoding='utf8') as fh:
+    long_description = fh.read()
+
+package_dir = {
+    'cloudreveimport': 'cloudreveimport',
+}
+
+setup(
+    name='cloudreveimport',
+    version='0.0.1',
+    author='yindaheng98',
+    author_email='yindaheng98@gmail.com',
+    url='https://github.com/yindaheng98/cloudreveimport',
+    description=u'Import your existing files into cloudreve!',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir=package_dir,
+    packages=[key for key in package_dir],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
