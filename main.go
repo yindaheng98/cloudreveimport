90c77f7bcadb72848b886464a6c035034e02d882
yindaheng98 <yindaheng98@163.com>
2024-02-11 19:31:34 -0800

    read files

--- main.go
+++ main.go
@@ -1,22 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"os"
 
 	model "github.com/cloudreve/Cloudreve/v3/models"
 	"github.com/cloudreve/Cloudreve/v3/pkg/conf"
 	"github.com/cloudreve/Cloudreve/v3/pkg/util"
+	ci "github.com/yindaheng98/cloudreveimport/util"
 )
 
 var (
 	userName string
 	confPath string
+	dataPath string
 )
 
 func init() {
 	flag.StringVar(&confPath, "c", util.RelativePath("conf.ini"), "Path to the config file.")
 	flag.StringVar(&userName, "u", "admin@cloudreve.org", "Email of the target user.")
+	flag.StringVar(&dataPath, "m", "", "Map of files to be imported.")
 	flag.Parse()
 	conf.Init(confPath)
 	model.Init()
@@ -27,5 +32,22 @@ func main() {
 	if err != nil {
 		return
 	}
-	util.Log().Info(fmt.Sprintf("%v\n", user))
+	util.Log().Info(fmt.Sprintf("User: %+v\n", user))
+	file, err := os.Open(dataPath)
+	if err != nil {
+		util.Log().Error("%+v", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		data := scanner.Bytes()
+		v, err := ci.Unmarshal(data)
+		if err != nil {
+			util.Log().Error("%s %+v", string(data), err)
+			continue
+		}
+		util.Log().Info("%+v", v)
+	}
 }
