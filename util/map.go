7218aeafc93bc6e5f9517b8adb487c7567489eb9
yindaheng98 <yindaheng98@163.com>
2024-02-11 20:43:56 -0800

    more commands

--- util/map.go
+++ util/map.go
@@ -1,20 +1,45 @@
 package util
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"time"
+)
 
-type FileMap struct {
-	SourceName string   `json:"source_name"`
-	DstPath    []string `json:"dst_path"`
+const (
+	ImportFileCommand       = "ImportFile"
+	UpdateFileTimeCommand   = "UpdateFileTime"
+	UpdateFolderTimeCommand = "UpdateFolderTime"
+)
+
+type Command struct {
+	Command            string    `json:"command"`
+	SourceName         string    `json:"source_name"`
+	DstPath            []string  `json:"dst_path"`
+	CreatedAtTimestamp int64     `json:"created_at"`
+	UpdatedAtTimestamp int64     `json:"updated_at"`
+	CreatedAt          time.Time `json:"-"`
+	UpdatedAt          time.Time `json:"-"`
 }
 
-func Marshal(v FileMap) ([]byte, error) {
+func max(a, b int64) int64 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func Marshal(v Command) ([]byte, error) {
+	v.CreatedAtTimestamp = max(v.CreatedAt.Unix(), 0)
+	v.UpdatedAtTimestamp = max(v.UpdatedAt.Unix(), 0)
 	return json.Marshal(v)
 }
 
-func Unmarshal(data []byte) (*FileMap, error) {
-	v := &FileMap{}
+func Unmarshal(data []byte) (*Command, error) {
+	v := &Command{}
 	if err := json.Unmarshal(data, v); err != nil {
 		return nil, err
 	}
+	v.CreatedAt = time.Unix(v.CreatedAtTimestamp, 0)
+	v.UpdatedAt = time.Unix(v.UpdatedAtTimestamp, 0)
 	return v, nil
 }
--- util/map_test.go
+++ util/map_test.go
@@ -3,19 +3,22 @@ package util
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 func Test_Marshal(t *testing.T) {
-	data, _ := Marshal(FileMap{
+	data, _ := Marshal(Command{
+		Command:    UpdateFileTimeCommand,
 		SourceName: "/gallery/data/twitter/eumi_114/1426380913575743488_1.jpg",
 		DstPath:    []string{"www", "ggg", "ggg", "iii.png"},
+		CreatedAt:  time.Now(),
 	})
 	t.Log(string(data))
 }
 
 func Test_Unmarshal(t *testing.T) {
 	v, _ := Unmarshal(
-		[]byte("{\"source_name\":\"/gallery/data/twitter/eumi_114/1426380913575743488_1.jpg\",\"dst_path\":[\"www\",\"ggg\",\"ggg\",\"iii.png\"]}"),
+		[]byte(`{"command":"UpdateFileTime","source_name":"/gallery/data/twitter/eumi_114/1426380913575743488_1.jpg","dst_path":["www","ggg","ggg","iii.png"],"created_at":1707712939,"updated_at":0}`),
 	)
 	t.Log(fmt.Sprintf("%+v", v))
 }
